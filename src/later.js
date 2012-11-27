4624ce799f7b6d7cd5cbfd6f6636a042be2b0d77
Grzegorz Tanczyk <fb@tanczyk.pl>
2012-11-27 22:13:59 +0100

    author

--- src/later.js
+++ src/later.js
@@ -1,7 +1,7 @@
 /**
  * Returns function which invocation will be delayed by given timeout value(ms) 
  * 
- * @ Grzegorz Tañczyk
+ * @author Grzegorz Tañczyk
  * @param {number} timeout Delay in milliseconds.
  * @param {Object} scope Scope used for invocation.
  * @return {Function} The delayed version of function.
